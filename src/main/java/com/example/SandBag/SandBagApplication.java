80a0f4066eeb6ad65043f9201deaa6c2041e6521
manal <khalilim387@gmail.com>
2025-03-11 15:03:59 +0200

    Initial commit

---
+++ src/main/java/com/example/SandBag/SandBagApplication.java
@@ -0,0 +1,13 @@
+package com.example.SandBag;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SandBagApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SandBagApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SandBag/SandBagApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SandBag;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SandBagApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
